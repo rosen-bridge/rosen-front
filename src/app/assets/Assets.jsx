d69fe5c0206e96a10f706436d79001105a5d11bc
Mojtaba Fazelinia <mfazelinia@gmail.com>
2022-07-29 14:16:23 +0430

    fixes assets data

--- src/app/assets/Assets.jsx
+++ src/app/assets/Assets.jsx
@@ -4,6 +4,7 @@ import {Table, TableContainer, TableHead, TableRow, TableCell, Typography, Table
 import {styled} from "@mui/material/styles";
 import NumberFormat from 'react-number-format';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import {assetsData} from "../data";
 
 const ContainerBox = styled(TableContainer) (({ theme }) => `
     & th {
@@ -18,20 +19,7 @@ export default function Assets() {
     const [assets, set_assets] = useState([])
 
     useEffect(() => {
-        set_assets([
-            {symbol: "WETH", name: "Wrapped Ether", type: "ERC721", originAddress: "0xC02aaA39...083C756Cc2", mappingAddress: "0x8aB252b5...0923A477b3", totalLocked: 5835305343958081000000000},
-            {symbol: "COMFY", name: "ComfyLori", type: "ERC20", originAddress: "0xbE335964...e4b16D51a4", mappingAddress: "0xB2947bd3...600A5ED658", totalLocked: 100000000000},
-            {symbol: "WETH", name: "Wrapped Ether", type: "ERC721", originAddress: "0xC02aaA39...083C756Cc2", mappingAddress: "0x8aB252b5...0923A477b3", totalLocked: 5835305343958081000000000},
-            {symbol: "COMFY", name: "ComfyLori", type: "ERC20", originAddress: "0xbE335964...e4b16D51a4", mappingAddress: "0xB2947bd3...600A5ED658", totalLocked: 100000000000, totalLockedUsd: 4222646000},
-            {symbol: "WETH", name: "Wrapped Ether", type: "ERC721", originAddress: "0xC02aaA39...083C756Cc2", mappingAddress: "0x8aB252b5...0923A477b3", totalLocked: 5835305343958081000000000},
-            {symbol: "COMFY", name: "ComfyLori", type: "ERC20", originAddress: "0xbE335964...e4b16D51a4", mappingAddress: "0xB2947bd3...600A5ED658", totalLocked: 100000000000},
-            {symbol: "WETH", name: "Wrapped Ether", type: "ERC721", originAddress: "0xC02aaA39...083C756Cc2", mappingAddress: "0x8aB252b5...0923A477b3", totalLocked: 5835305343958081000000000},
-            {symbol: "COMFY", name: "ComfyLori", type: "ERC20", originAddress: "0xbE335964...e4b16D51a4", mappingAddress: "0xB2947bd3...600A5ED658", totalLocked: 100000000000},
-            {symbol: "WETH", name: "Wrapped Ether", type: "ERC721", originAddress: "0xC02aaA39...083C756Cc2", mappingAddress: "0x8aB252b5...0923A477b3", totalLocked: 5835305343958081000000000},
-            {symbol: "COMFY", name: "ComfyLori", type: "ERC20", originAddress: "0xbE335964...e4b16D51a4", mappingAddress: "0xB2947bd3...600A5ED658", totalLocked: 100000000000},
-            {symbol: "WETH", name: "Wrapped Ether", type: "ERC721", originAddress: "0xC02aaA39...083C756Cc2", mappingAddress: "0x8aB252b5...0923A477b3", totalLocked: 5835305343958081000000000},
-            {symbol: "COMFY", name: "ComfyLori", type: "ERC20", originAddress: "0xbE335964...e4b16D51a4", mappingAddress: "0xB2947bd3...600A5ED658", totalLocked: 100000000000},
-        ])
+        set_assets(assetsData)
     },[])
 
     return (
