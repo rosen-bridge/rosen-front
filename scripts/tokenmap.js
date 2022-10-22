98f3b443bf08b8630caba454f93de4beb54fc02f
SepehrGanji <sepehrganji79@gmail.com>
2022-10-22 16:26:25 +0330

    fetch release_tag from github

--- scripts/tokenmap.js
+++ scripts/tokenmap.js
@@ -2,18 +2,21 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 
 const tokenMapConfig = require("../src/configs/remote.json");
-
 const githubRepo = tokenMapConfig.tokenmap.github_repo;
-const releaseTag = tokenMapConfig.tokenmap.release_tag;
-const pureVersion = releaseTag.replace("v", "");
-const url = `https://github.com/${githubRepo}/releases/download/${releaseTag}/tokensmap-${pureVersion}.json`;
-let settings = { method: "Get" };
 
-fetch(url, settings)
-    .then(res => res.json())
+let settings = { method: "Get" };
+fetch(`https://api.github.com/repos/${githubRepo}/releases?per_page=1`, settings)
+    .then((res) => res.json())
     .then((json) => {
-        const path = __dirname.split("/");
-        path.pop();
-        const filePath = path.join("/") + "/src/configs/tokenmap.json";
-        fs.writeFileSync(filePath, JSON.stringify(json, null, 4));
+        const releaseTag = json[0].tag_name;
+        const pureVersion = releaseTag.replace("v", "");
+        const url = `https://github.com/${githubRepo}/releases/download/${releaseTag}/tokensmap-${pureVersion}.json`;
+        fetch(url, settings)
+            .then((res) => res.json())
+            .then((json) => {
+                const path = __dirname.split("/");
+                path.pop();
+                const filePath = path.join("/") + "/src/configs/tokenmap.json";
+                fs.writeFileSync(filePath, JSON.stringify(json, null, 4));
+            });
     });
